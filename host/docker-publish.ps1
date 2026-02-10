d4688b58c8433a3417ba5a8ce7bf5f011ef780a2
Lukiya Chen <lukiya@outlook.com>
2026-02-10 08:42:14 -0800

    Update docker-publish.ps1 to include version tagging for Docker images; add support for version 1.0.5 and push images with both 'latest' and version-specific tags.

--- host/docker-publish.ps1
+++ host/docker-publish.ps1
@@ -1,14 +1,18 @@
 # !!!!! Make sure ./build.ps1 already executed without problem first
 
 $imageName = "logs"
+$version = "1.0.5"
 $targetDir = "./dist"
 
 Write-Host "#: loading docker image"
 docker load -i $targetDir/$imageName.tar
-docker tag $imageName dreamvat/$imageName
-Write-Host "#: pushing docker image"
-docker push dreamvat/$imageName
+docker tag $imageName dreamvat/$imageName`:latest
+docker tag $imageName dreamvat/$imageName`:$version
+Write-Host "#: pushing docker image with tags: latest, $version"
+docker push dreamvat/$imageName`:latest
+docker push dreamvat/$imageName`:$version
 Write-Host "#: clear temperary files..."
-docker rmi dreamvat/$imageName
+docker rmi dreamvat/$imageName`:latest
+docker rmi dreamvat/$imageName`:$version
 docker rmi $imageName
 Write-Host "#: done"
